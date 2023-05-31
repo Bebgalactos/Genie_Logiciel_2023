362cc19b714be5e3570ba3ba7f415400b6b8f76f
Lucas Bertrand-Christen <l.b-c@hotmail.com>
2023-05-31 15:08:51 +0200

    tests-timecount-LBC

--- src/main/java/fr/ul/miage/bipwac/gl/metro/main/TimeCount.java
+++ src/main/java/fr/ul/miage/bipwac/gl/metro/main/TimeCount.java
@@ -2,20 +2,31 @@ package fr.ul.miage.bipwac.gl.metro.main;
 
 import fr.ul.miage.bipwac.gl.metro.graphe.Edge;
 import fr.ul.miage.bipwac.gl.metro.graphe.MetroParisien;
+import fr.ul.miage.bipwac.gl.metro.graphe.Node;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TimeCount {
 
+    // Variables
+    public static final Double averageTravelTime = 1.5; // Temps moyen entre stations
+    public static final Double averageTransitionEntryTime = 5.0; // Temps moyen de changements et de première entrée
+
+    // Constructeur
+    public TimeCount() {}
+
+    // Fonctions
     public Double getTimeInMinutes(MetroParisien metro) {
         // Variables
-        Double time = 0.0;
-        Double averageTravelTime = 1.5; // Temps moyen entre stations
-        Double averageTransitionTime = 5.0; // Temps moyen de changements et de première entrée
+        double time = 0.0;
+
+        // Vérification de la cohérence du chemin en entrée
+        if(!verifyCoherence(metro)){
+            return null;
+        }
 
         // Pour chaque station
-        for(Edge edge : metro.getEdges())  {
+        for(Edge ignored : metro.getEdges())  {
             time += averageTravelTime;
         }
 
@@ -29,8 +40,84 @@ public class TimeCount {
         int numberTransitions = listDifferentLines.size();
 
         // Ajout des temps de transitions de ligne
-        time += numberTransitions * averageTransitionTime;
+        time += numberTransitions * averageTransitionEntryTime;
 
         return time;
+    }
+
+    private boolean verifyCoherence(MetroParisien metro) {
+        List<Node> nodes = metro.getNodes();
+        for(Node node : nodes) {
+            for(Node node2 : nodes) {
+                if(!String.valueOf(node.getId()).equals(String.valueOf(node2.getId()))) {
+                    System.out.println(node.getId()  + " vers " + node2.getId() + " : " + isReachable(metro, node.getId(), node2.getId()));
+                    if(!isReachable(metro, node.getId(), node2.getId())){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isReachable(MetroParisien metro, Long startNodeId, Long targetNodeId) {
+        // Vérification des nœuds de départ et d'arrivée
+        Node startNode = getNodeById(metro, startNodeId, startNodeId).get(0);
+        Node targetNode = getNodeById(metro, targetNodeId, targetNodeId).get(0);
+        if (startNode == null || targetNode == null) {
+            return false; // L'un des nœuds n'existe pas dans le graphe
+        }
+
+        // Utilisation d'une pile pour la recherche en profondeur
+        Deque<Node> stack = new ArrayDeque<>();
+        Set<Long> visited = new HashSet<>();
+
+        // Ajouter le nœud de départ à la pile
+        stack.push(startNode);
+
+        while (!stack.isEmpty()) {
+            Node currentNode = stack.pop();
+
+            if (currentNode.getId() == targetNodeId) {
+                return true; // Le nœud cible a été trouvé
+            }
+
+            if (!visited.contains(currentNode.getId())) {
+                visited.add(currentNode.getId());
+
+                // Explorer les nœuds voisins non visités
+                List<Edge> edges = getAdjacentEdges(metro, currentNode.getId());
+                for (Edge edge : edges) {
+                    List<Node> neighbors = getNodeById(metro, edge.getTarget(), edge.getSource());
+                    for(Node neighbor : neighbors) {
+                        if (!visited.contains(neighbor.getId())) {
+                            stack.push(neighbor);
+                        }
+                    }
+                }
+            }
+        }
+
+        return false; // Le nœud cible n'a pas été trouvé
+    }
+
+    private List<Node> getNodeById(MetroParisien metro, long nodeTargetId, long nodeSourceId) {
+        List<Node> nodes = new ArrayList<>();
+        for (Node node : metro.getNodes()) {
+            if (node.getId() == nodeTargetId || node.getId() == nodeSourceId) {
+                nodes.add(node);
+            }
+        }
+        return nodes;
+    }
+
+    private List<Edge> getAdjacentEdges(MetroParisien metro, long nodeId) {
+        List<Edge> edges = new ArrayList<>();
+        for (Edge edge : metro.getEdges()) {
+            if (edge.getSource() == nodeId || edge.getTarget() == nodeId) {
+                edges.add(edge);
+            }
+        }
+        return edges;
     }
 }
--- src/test/java/fr/ul/miage/bipwac/gl/metro/main/TimeCountTest.java
+++ src/test/java/fr/ul/miage/bipwac/gl/metro/main/TimeCountTest.java
@@ -1,9 +1,105 @@
 package fr.ul.miage.bipwac.gl.metro.main;
 
+import fr.ul.miage.bipwac.gl.metro.graphe.Edge;
+import fr.ul.miage.bipwac.gl.metro.graphe.MetroParisien;
+import fr.ul.miage.bipwac.gl.metro.graphe.Node;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static fr.ul.miage.bipwac.gl.metro.main.TimeCount.*;
+
 public class TimeCountTest {
 
     @Test
     public void testManuallyCreatedGraph() {
+        // Variables
+        String line1 = "ligne1";
+        String line2 = "ligne2";
+
+        // Premier branchement
         Node node1 = new Node();
+        node1.setId(Long.parseLong("1"));
+        Node node2 = new Node();
+        node2.setId(Long.parseLong("2"));
+        Edge edge1 = new Edge();
+        edge1.setSource(node1.getId());
+        edge1.setTarget(node2.getId());
+        edge1.setLine(line1);
+
+        Node node3 = new Node();
+        node3.setId(Long.parseLong("3"));
+        Edge edge2 = new Edge();
+        edge2.setSource(node2.getId());
+        edge2.setTarget(node3.getId());
+        edge2.setLine(line1);
+
+        Node node4 = new Node();
+        node4.setId(Long.parseLong("4"));
+        Edge edge3 = new Edge();
+        edge3.setSource(node3.getId());
+        edge3.setTarget(node4.getId());
+        edge3.setLine(line2);
+
+        List<Node> nodes = new ArrayList<>(Arrays.asList(node1, node2, node3, node4));
+        List<Edge> edges = new ArrayList<>(Arrays.asList(edge1, edge2, edge3));
+        MetroParisien metro = new MetroParisien();
+        metro.setNodes(nodes);
+        metro.setEdges(edges);
+        /*
+        On obtient le trajet :
+            1 -------ligne1------- 2 -------ligne1------- 3 -------ligne2------- 4
+        Soit Un passage dans 4 stations et 2 lignes différentes
+         */
+        // On cherche à calculer 3 déplacements entre station et 2 changements de ligne (entrée en l1 et passage en l2)
+        Double expected = (3.0 * averageTravelTime + 2.0 * averageTransitionEntryTime);
+        Double actual = (new TimeCount()).getTimeInMinutes(metro);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testBadlyCreatedGraph() {
+        // Variables
+        String line1 = "ligne1";
+        String line2 = "ligne2";
+
+        // Premier branchement
+        Node node1 = new Node();
+        node1.setId(Long.parseLong("1"));
+        Node node2 = new Node();
+        node2.setId(Long.parseLong("2"));
+        Edge edge1 = new Edge();
+        edge1.setSource(node1.getId());
+        edge1.setTarget(node2.getId());
+        edge1.setLine(line1);
+
+        Node node3 = new Node();
+        node3.setId(Long.parseLong("3"));
+        Edge edge2 = new Edge();
+        edge2.setSource(node2.getId());
+        edge2.setTarget(node3.getId());
+        edge2.setLine(line1);
+
+        Node node4 = new Node();
+        node4.setId(Long.parseLong("4"));
+        Edge edge3 = new Edge();
+        edge3.setSource(Long.valueOf("5")); // Edge mal relié part de 5 au lieu de 3.
+        edge3.setTarget(node4.getId());
+        edge3.setLine(line2);
+
+        List<Node> nodes = new ArrayList<>(Arrays.asList(node1, node2, node3, node4));
+        List<Edge> edges = new ArrayList<>(Arrays.asList(edge1, edge2, edge3));
+        MetroParisien metro = new MetroParisien();
+        metro.setNodes(nodes);
+        metro.setEdges(edges);
+        /*
+        On obtient le trajet :
+            1 -------ligne1------- 2 -------ligne1------- 3 -------ligne2------- 4
+        Soit Un passage dans 4 stations et 2 lignes différentes
+         */
+        // On cherche à calculer 3 déplacements entre station et 2 changements de ligne (entrée en l1 et passage en l2)
+        assertNull((new TimeCount()).getTimeInMinutes(metro));
     }
 }
