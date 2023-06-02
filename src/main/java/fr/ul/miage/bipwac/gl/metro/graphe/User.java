fec0018c9462b425926b91ed1dce896e9e97d52e
NthCourtin <nth.courtin@gmail.com>
2023-06-02 17:24:42 +0200

    Ajout comment

--- src/main/java/fr/ul/miage/bipwac/gl/metro/graphe/User.java
+++ src/main/java/fr/ul/miage/bipwac/gl/metro/graphe/User.java
@@ -2,6 +2,9 @@ package fr.ul.miage.bipwac.gl.metro.graphe;
 
 import java.util.Random;
 
+/**
+ * Class gerant l'utilisateur
+ */
 public class User {
 
     private double userLatitude;
@@ -14,24 +17,41 @@ public class User {
     Longitude maximum : 2.469920
      */
 
+    /**
+     * constructeur
+     */
     public User(){
         getLatitudeRandom();
         getLongitudeRandom();
     }
 
+    /**
+     * getteur userLongitude
+     * @return double
+     */
     public double getUserLongitude() {
         return userLongitude;
     }
 
+    /**
+     * getteur userLatitude
+     * @return double
+     */
     public double getUserLatitude() {
         return userLatitude;
     }
 
+    /**
+     * genere un random longitude entre les limites
+     */
     public void getLongitudeRandom(){
         Random r = new Random();
         this.userLongitude = 2.225828 + (2.469920 - 2.225828) * r.nextDouble();
     }
 
+    /**
+     * genere un random latitude entre les limites
+     */
     public void getLatitudeRandom(){
         Random r = new Random();
         this.userLatitude = 48.815573 + (48.902145 - 48.815573) * r.nextDouble();
